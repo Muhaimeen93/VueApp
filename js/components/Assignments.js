58925785ff0c1eb1cebda0b8accfbc6fda1766bd
Muhaimeen Chowdhury <schy92@gmail.com>
2022-09-21 16:02:39 +0000

    Event handling added

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,12 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            <assignment-create @add="add"></assignment-create>
+            
         </section>
     `,
 
@@ -17,17 +20,29 @@ export default {
         { name: "Task 2", complete: false, id: 2 },
         { name: "Task 3", complete: false, id: 3 },
       ],
+     
     };
   },
 
   computed: {
     filters() {
       return {
-        inProgress: this.assignments.filter(
-          (assignment) => !assignment.complete
-        ),
+        inProgress: this.assignments.filter((assignment) => !assignment.complete),
         completed: this.assignments.filter((assignment) => assignment.complete),
       };
-    },
+    }
   },
+
+  methods: {
+    add(name) {
+      if (name.trim() != '') {
+        this.assignments.push({
+          name: name,
+          complete: false,
+          id: this.assignments.length + 1
+        });
+      }
+    }
+  }
+
 };
