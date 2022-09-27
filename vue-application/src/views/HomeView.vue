38b9aef2bb1d38ee4fe80646b9d1bdf73a6d47bd
Muhaimeen Chowdhury <schy92@gmail.com>
2022-09-27 20:01:08 +0000

    vite setup done-new app created

---
+++ vue-application/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+import { ref} from "vue";
+let message = ref("Hello World");
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{message}}</p>
+
+    <p>
+      <input type="text" v-model="message"/>
+    </p>
+  </main>
+</template>
